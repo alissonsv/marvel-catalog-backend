3ea65adf702c3a24dc7dd64294b9f4277959e3bc
Alisson Vargas <alisson.vargass@hotmail.com>
2021-05-27 08:34:22 -0300

    feat: add comics and characters routers

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ require('dotenv-flow').config({ path: path.join(__dirname, './config') });
 
 const express = require('express');
 const userRouter = require('./routers/user');
+const charactersRouter = require('./routers/characters');
+const comicsRouter = require('./routers/comics');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/user/', userRouter);
+app.use('/api/characters/', charactersRouter);
+app.use('/api/comics/', comicsRouter);
 
 module.exports = app;
---
+++ src/routers/characters.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const md5 = require('md5');
+const fetch = require('node-fetch');
+
+const auth = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/', auth, async (req, res) => {
+  const url = new URL('https://gateway.marvel.com:443/v1/public/characters');
+  const ts = Date.now();
+  const publicKey = process.env.MARVEL_PUBLIC_KEY;
+  const privateKey = process.env.MARVEL_PRIVATE_KEY;
+  const hash = md5(ts + privateKey + publicKey);
+
+  url.searchParams.set('ts', ts);
+  url.searchParams.set('apikey', publicKey);
+  url.searchParams.set('hash', hash);
+
+  const params = Object.keys(req.query);
+
+  params.forEach((param) => url.searchParams.set(param, req.query[param]));
+
+  try {
+    const response = await fetch(url);
+    const characters = await response.json();
+
+    if (characters.code !== 200) {
+      throw new Error({ error: 'Error at Marvel API' });
+    }
+
+    res.send(characters);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+module.exports = router;
