9ca56a396d011ff7923683f918a4e4e7a4f108e4
Alisson Vargas <alisson.vargass@hotmail.com>
2021-05-31 01:17:53 -0300

    refactor: add cors

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const path = require('path');
 require('dotenv-flow').config({ path: path.join(__dirname, '../config') });
 
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routers/user');
 const charactersRouter = require('./routers/characters');
 const comicsRouter = require('./routers/comics');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/user/', userRouter);
 app.use('/api/characters/', charactersRouter);
